d67a82599b8535450cd56d49858ac730474c3b76
PhantomVasploit <paulvasgit99@gmail.com>
2023-04-19 12:34:55 +0300

    Project initialization

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -8,7 +8,7 @@ const sequelize = new Sequelize(
     process.env.DATABASE_PASSWORD, 
     { 
         host: process.env.DATABASE_HOST,
-        dialect: "mssql"
+        dialect: process.env.DATABASE_DIALECT
     });
 
 
