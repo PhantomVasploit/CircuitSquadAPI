241a14766e250f77e3617345980030561d95874e
PhantomVasploit <paulvasgit99@gmail.com>
2023-04-19 13:44:17 +0300

    Project finalization

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -7,7 +7,7 @@ const sequelize = new Sequelize(
     process.env.DATABASE_USERNAME, 
     process.env.DATABASE_PASSWORD, 
     { 
-        host: process.env.DATABASE_HOST,
+        host: process.env.DATABASE_SERVER_NAME,
         dialect: process.env.DATABASE_DIALECT
     });
 
